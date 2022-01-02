fc7a05dd9ebbcbdeef017fee4a4f5e121cf79fa3
김하경 <khg05311@naver.com>
2022-01-02 17:38:40 +0900

    1:44:19

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -40,7 +40,7 @@ const Home = () => {
             <img src={logo} alt="logo" className='w-28' />
           </Link>
           <Link to={`user-profile/${user?._id}`}>
-            <img src={user?.image} alt="logo" className='w-28 rounded-lg' />
+            <img src={user?.image} alt="logo" className='w-28 rounded-full' />
           </Link>
         </div>
         {toggleSidebar && (
